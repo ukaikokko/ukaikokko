8f08f16bf2c1ae0a21b262eacba885c39d539fb6
ygdstmidn <121283845+ygdstmidn@users.noreply.github.com>
2026-08-22 15:43:41 +0900

    💬 StartとStopの意味を説明する

--- LambchanMD/LambchanMD.h
+++ LambchanMD/LambchanMD.h
@@ -22,7 +22,9 @@ class LambchanMD
     LambchanMD(PWM pwm, GPOutput dir, MDParam param)
         : _pwm(pwm), _dir(dir), _param(param) {}
     ~LambchanMD() = default;
+    /// @brief PWM出力を開始する
     HAL_StatusTypeDef start() const;
+    /// @brief PWM出力を停止し，DirピンをLowにする
     HAL_StatusTypeDef stop() const;
     /// @param output 範囲 : -1.0 ~ 1.0
     double setOutput(const double output);
