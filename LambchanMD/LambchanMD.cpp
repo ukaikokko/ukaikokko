713c684ee1f696a98006205d294efe9839576ae6
ygdstmidn <121283845+ygdstmidn@users.noreply.github.com>
2026-08-22 13:15:13 +0900

    🚧 開発中(動かない)

---
+++ LambchanMD/LambchanMD.cpp
@@ -0,0 +1,66 @@
+#include "LambchanMD.h"
+
+namespace ukaikokko
+{
+
+LambchanMD::~LambchanMD()
+{
+    stop();
+}
+
+HAL_StatusTypeDef LambchanMD::start() const
+{
+    _dir.write(GPIO_PIN_RESET);
+    return _pwm.start();
+}
+
+HAL_StatusTypeDef LambchanMD::stop() const
+{
+    _dir.write(GPIO_PIN_RESET);
+    return _pwm.stop();
+}
+
+double LambchanMD::setOutput(const double output)
+{
+    double targetOutput = _output * _param.direction; // 方向補正
+    // 最大出力clamp
+    if (targetOutput > _param.maxOutput)
+    {
+        targetOutput = _param.maxOutput;
+    }
+    else if (targetOutput < _param.minOutput)
+    {
+        targetOutput = _param.minOutput;
+    }
+    // 最大加速度clamp
+    const double nextMaxOutput = _output + _param.maxAccel;
+    const double nextMinOutput = _output - _param.maxAccel;
+    if (targetOutput > nextMaxOutput)
+    {
+        targetOutput = nextMaxOutput;
+    }
+    else if (targetOutput < nextMinOutput)
+    {
+        targetOutput = nextMinOutput;
+    }
+
+    _output = targetOutput;
+    forceOutput();
+    return _output;
+}
+
+void LambchanMD::forceOutput()
+{
+    if (_output < 0.0)
+    {
+        _dir.write(GPIO_PIN_RESET);
+        _pwm.setDuty(-_output);
+    }
+    else
+    {
+        _dir.write(GPIO_PIN_SET);
+        _pwm.setDuty(_output);
+    }
+}
+
+} // namespace ukaikokko
---
+++ LambchanMD/LambchanMD.h
@@ -0,0 +1,38 @@
+#ifndef __UKAIKOKKO_LAMBCHAN_MD_H
+#define __UKAIKOKKO_LAMBCHAN_MD_H
+
+#include <main.h>
+#include "../GPIO/GPOutput.h"
+#include "../PWM/PWM.h"
+
+namespace ukaikokko
+{
+
+struct MDParam
+{
+    int direction = 1;       // 1:正転, -1:逆転
+    double maxOutput = 1.0;  // 最大出力
+    double minOutput = -1.0; // 最小出力
+    double maxAccel = 0.1;   // 最大加速度(output_diff/呼び出し周期)
+};
+
+class LambchanMD
+{
+   public:
+    LambchanMD(ukaikokko::PWM pwm, ukaikokko::GPOutput dir, ukaikokko::MDParam param) : _pwm(pwm), _dir(dir), _param(param) {}
+    ~LambchanMD();
+    HAL_StatusTypeDef start() const;
+    HAL_StatusTypeDef stop() const;
+    double setOutput(const double output); // @param output 範囲 : -10.0 ~ 1.0
+
+   private:
+    void forceOutput();
+    ukaikokko::PWM _pwm;
+    ukaikokko::GPOutput _dir;
+    ukaikokko::MDParam _param;
+    double _output = 0.0;
+};
+
+} // namespace ukaikokko
+
+#endif // __UKAIKOKKO_LAMBCHAN_MD_H
