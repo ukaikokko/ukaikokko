43cf4c0ae752591a94a3c4eb3f0bd27c49d46586
ygdstmidn <121283845+ygdstmidn@users.noreply.github.com>
2026-09-09 20:09:13 +0900

    🩹 💬 コメントの位置を変えた(ほとんど影響はない)

--- AnalogInputDMA/AnalogInputDMA.cpp
+++ AnalogInputDMA/AnalogInputDMA.cpp
@@ -6,9 +6,9 @@ namespace ukaikokko
 HAL_StatusTypeDef AnalogInputDMA::start() const
 {
     HAL_StatusTypeDef status = HAL_ADC_Start_DMA(_hadc, _dma_buf, _length);
-    // 想定するDMAのバッファはちょうどlengthぶんしかないので，コールバックは不要
     if (status == HAL_OK)
     {
+        // 想定するDMAのバッファはちょうどlengthぶんしかないので，コールバックは不要
         __HAL_DMA_DISABLE_IT(_hadc->DMA_Handle, DMA_IT_HT); // half transfer 無効にしてCPU負荷を減らす
         __HAL_DMA_DISABLE_IT(_hadc->DMA_Handle, DMA_IT_TC); // transfer complete 無効にしてCPU負荷を減らす
     }
